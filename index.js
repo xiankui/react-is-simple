09f0cbb262d4a63bdbfcdeea0da5eeb999a91a4c
zhuxiankui <zhuxiankui@zhuxiankuis-MacBook-Pro.local>
2017-06-01 12:03:23 +0800

    handling-events

--- index.js
+++ index.js
@@ -4,84 +4,48 @@
  */
 
 /**
- * 通过状态组件维护UI的变化，而不是每次都要调用ReactDOM.render()
- * 当组件state变化时，当前组件树就会重新render
+ * react handling events
  */
-class Clock extends React.Component {
-	constructor(props) {
+class Toggle extends React.Component {
+  constructor(props) {
     super(props);
+  
+    this.state = {isToggleOn: false};
 
-    // state 初始化
-    this.state = {date: new Date()};
+    // This binding is necessary to make `this` work in the callback; otherwise, you can use arrow function
+    this.handleClick = this.handleClick.bind(this);
   }
 
-	/**
-	 * 组件挂载
-	 */
-  componentWillMount() {
-  	console.log('componentWillMount')
-  }
-
+  // when componentDidMount, mock handleClick
   componentDidMount() {
-  	console.log('componentDidMount')
-  	console.log(this.state.date.toLocaleTimeString())
-  	this.timerID = setTimeout(() => this.tick(), 1000)
+    // but, no 'e' target
+    this.handleClick()
   }
 
-	/**
-	 * 组件更新
-	 */
-  componentWillReceiveProps(nextProps) {
-  	// 作为子组件被嵌套时，可能会触发此状态
-  	console.log('componentWillReceiveProps')
+  handleClick(e) {
+    console.log(e)
+
+    // 阻止默认行为；比如a链接跳转
+    e && e.preventDefault();
+
+    // when bind to component-self, this is component-self; otherwise, this is null
+    console.log('this', this)
+
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }))
   }
 
-  shouldComponentUpdate(nextProps, nextState) {
-  	// 组件需要更新吗？默认需要更新
-  	// PureComponent会进行一次浅比较，可结合immutable.js
-  	// 一般有针对性的深比较一次即可，比如只比较id
-  	console.log('shouldComponentUpdate')
-  	return true;
+  render() {
+    return (
+      <button style={{padding: '5px 20px'}} onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    )
   }
-
-  componentWillUpdate() {
-  	console.log('componentWillUpdate')
-  }
-
-  componentDidUpdate() {
-  	console.log('componentDidUpdate')
-  }
-
-	/** 
-	 * 组件卸载
-	 */
-  componentWillUnmout() {
-  	clearTimeout(this.timerID)
-  }
-
-  tick() {
-  	// 更改state
-  	// this.setState({
-  	//		date: new Date()
-  	// })
-
-		// 更加安全的方式
-  	this.setState((prevState, props) => ({
-  		date: new Date()
-  	}))
-  }
-
-	render() {
-		return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-	}
 }
 
 ReactDOM.render(
-	<Clock />,
+	<Toggle />,
 	document.getElementById('root')
 )
