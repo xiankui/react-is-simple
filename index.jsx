dd93a9eadeef66eed244e5e7f7040c368f3442ac
zhuxiankui <zhuxiankui@zhuxiankuis-MacBook-Pro.local>
2017-06-05 14:57:40 +0800

    fix

--- index.jsx
+++ index.jsx
@@ -10,7 +10,7 @@
 // 5. jsx removes whitespace at the beginning and ending of a line. It also removes blank lines. 
 // 6. jsx children can be any types: ['String', 'Array', 'Object', 'Function', 'Other Component', 'JavaScript Expressions that return jsx', '...']
 // 7. Booleans, Null, and Undefined Are Ignored
-// 8. in one word, any thing React.createElement can compiled correctly is valid.
+// 8. in one word, any thing React can compiled correctly is valid.
 
 function Hello(props) {
 	return <div>hello {props.toWhat}</div>
