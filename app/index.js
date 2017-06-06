732db7b3e79058592fe64c74c30acbddec27912d
zhuxiankui <zhuxiankui@zhuxiankuis-MacBook-Pro.local>
2017-06-06 14:10:08 +0800

    refs and the dom

--- app/index.js
+++ app/index.js
@@ -1,28 +1,46 @@
 /**
- * typechecking-with-proptypes
+ * refs-and-the-dom
  */
+
+// 0. React可以通过ref属性的callback function来获取原生DOM节点，以便操作
+// 1. ref属性的函数会在组件mounted or unmounted时立即执行
+// 2. 当然，除非特别必要，不要这样做！想一想是不是可以用state来完成！
  
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
 
-class Greeting extends Component {
+class CustomTextInput extends Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {};
+
+	  this.focus = this.focus.bind(this)
+	}
+
+	focus() {
+		this.textInput.focus()
+	}
+
   render() {
+  	// when component mounted, ref callback function executed immediately
     return (
-      <h1>Hello, {this.props.name}</h1>
+      <div>
+        <input
+          type="text"
+          ref={(input) => { this.textInput = input; }} />
+        <input
+          type="button"
+          value="Focus the text input"
+          onClick={this.focus}
+        />
+      </div>
     );
   }
 }
 
-Greeting.propTypes = {
-	name: PropTypes.string.isRequired
-}
-
-Greeting.defaultProps = {
-	name: 'World'
-}
 
 ReactDOM.render(
-	<Greeting />,
+	<CustomTextInput />,
 	document.getElementById('root')
 )
