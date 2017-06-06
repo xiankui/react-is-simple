691cd81abb9d47b388734e90a31110214e12bf9f
zhuxiankui <zhuxiankui@zhuxiankuis-MacBook-Pro.local>
2017-06-06 17:39:56 +0800

    reconciliation

--- app/index.js
+++ app/index.js
@@ -1,96 +1,53 @@
 /**
- * optimizing-performance.
+ * reconciliation -- dom-diff -- 算法实现原理
+ * When you use React, 
+ * at a single point in time you can think of the render() function as creating a tree of React elements.
+ * On the next state or props update, that render() function will return a different tree of React elements. 
+ * React then needs to figure out how to efficiently update the UI to match the most recent tree.
  */
-// 0. 发布时使用代码压缩
-// 1. When a component's props or state change, React decides whether an actual DOM update is necessary by comparing the newly returned element with the previously rendered one. When they are not equal, React will update the DOM.
-// 2. shouldComponentUpdate决定要不要进行比较，有没有必要触发render
-// 3. PureComponent会自动进行浅比较（shallow comparison)
-// 4. 注意数据的操作方式，要拷贝不要突变；否则，PureComponent时的浅比较是相等的，因为指向同一个对象
+
+// 0. 比较元素的类型，如果类型不同，则销毁、重建！！！
+// 1. DOM Elements 类型相同时，对不同部分作更新处理
+// 2. Component Elements 类型相同时，按照 lifecycle 执行更新
+// 3. 列表子元素用 key 作对照标识；key 只需在兄弟元素中唯一即可
  
-import React, { Component, PureComponent } from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class CounterButton extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {count: 1};
-  }
+class RootTree extends Component {
+	constructor(props) {
+	  super(props);
+	
+	  this.state = {
+	  	lists: ['one', 'two', 'there']
+	  };
+	}
 
-  // PureComponent will do shallow comparison about props and state auto
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   if (this.props.color !== nextProps.color) {
-  //     return true;
-  //   }
-  //   if (this.state.count !== nextState.count) {
-  //     return true;
-  //   }
-  //   return false;
-  // }
+	render() {
 
-  render() {
-    return (
-      <button
-        color={this.props.color}
-        onClick={() => this.setState(state => ({count: state.count + 1}))}>
-        Count: {this.state.count}
-      </button>
-    );
-  }
+		// 切记，尽量保持 React elements tree 结构相同；主体结构必须保持稳定
+
+		return (
+			<div className="wrapper">
+				<div className="banner">banner</div>
+				<div className="container">
+					<div className="main">
+						<h1>main content</h1>
+						<ul>
+							{
+								this.state.lists.map(list => <li key={list}>{list}</li>)
+							}
+						</ul>
+					</div>
+					<div className="sidebar">sidebar</div>
+				</div>
+			</div>
+		)
+	}
 }
-
-
-
-// class ListOfWords extends PureComponent {
-//   render() {
-//     return <div>{this.props.words.join(',')}</div>;
-//   }
-// }
-
-
-// 函数组件默认 extends React.Component
-function ListOfWords(props) {
-	return <div>{props.words.join(',')}</div>
-}
-
-class WordAdder extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      words: ['marklar']
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentDidUpdate() {
-  	console.log('componentDidUpdate')
-  }
-
-  handleClick() {
-    // This section is bad style and causes a bug
-    // const words = this.state.words;
-    // words.push('marklar');
-    // this.setState({words: words});
-
-    // The simplest way to avoid this problem is to avoid mutating values that you are using as props or state.
-    this.setState(prevState => ({
-    	// words: prevState.words.concat(['marklar'])
-    	words: [...prevState.words, 'marklar']
-    }))
-  }
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>add word</button>
-        <ListOfWords words={this.state.words} />
-      </div>
-    );
-  }
-}
-
 
 
 ReactDOM.render(
-	<WordAdder />,
+	<RootTree />,
 	document.getElementById('root')
 )
