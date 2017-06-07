1ab07eac98f6bbba1c969fb26226f36286ba00bf
zhuxiankui <zhuxiankui@zhuxiankuis-MacBook-Pro.local>
2017-06-07 11:15:04 +0800

    web components

--- app/index.js
+++ app/index.js
@@ -1,53 +1,37 @@
 /**
- * reconciliation -- dom-diff -- 算法实现原理
- * When you use React, 
- * at a single point in time you can think of the render() function as creating a tree of React elements.
- * On the next state or props update, that render() function will return a different tree of React elements. 
- * React then needs to figure out how to efficiently update the UI to match the most recent tree.
+ * Web Components
+ * Most people who use React don't use Web Components!!
  */
 
-// 0. 比较元素的类型，如果类型不同，则销毁、重建！！！
-// 1. DOM Elements 类型相同时，对不同部分作更新处理
-// 2. Component Elements 类型相同时，按照 lifecycle 执行更新
-// 3. 列表子元素用 key 作对照标识；key 只需在兄弟元素中唯一即可
- 
+
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-class RootTree extends Component {
-	constructor(props) {
-	  super(props);
-	
-	  this.state = {
-	  	lists: ['one', 'two', 'there']
-	  };
-	}
+// register Web Component with react
+const proto = Object.create(HTMLElement.prototype, {
+  attachedCallback: {
+    value: function() {
+      const mountPoint = document.createElement('span');
+      this.createShadowRoot().appendChild(mountPoint);
 
-	render() {
+      const name = this.getAttribute('name');
+      const url = 'https://www.google.com/search?q=' + encodeURIComponent(name);
+      ReactDOM.render(<a href={url} target="blank">{name}</a>, mountPoint);
+    }
+  }
+});
+document.registerElement('x-search', {prototype: proto});
 
-		// 切记，尽量保持 React elements tree 结构相同；主体结构必须保持稳定
 
-		return (
-			<div className="wrapper">
-				<div className="banner">banner</div>
-				<div className="container">
-					<div className="main">
-						<h1>main content</h1>
-						<ul>
-							{
-								this.state.lists.map(list => <li key={list}>{list}</li>)
-							}
-						</ul>
-					</div>
-					<div className="sidebar">sidebar</div>
-				</div>
-			</div>
-		)
-	}
+// using web component in react
+class HelloMessage extends React.Component {
+	// One common confusion is that Web Components use "class" instead of "className".
+  render() {
+    return <div className="div">Hello <x-search class="x-search" name={this.props.name}></x-search>!</div>;
+  }
 }
 
-
 ReactDOM.render(
-	<RootTree />,
+	<HelloMessage name='reactjs' />,
 	document.getElementById('root')
 )
